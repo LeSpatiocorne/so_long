67c966c5505725a1994eec5c52f48c07d8c5ee7e
LeSpatiocorne <n2az.yt@gmail.com>
2025-04-28 18:08:17 +0200

    norm s1

--- src/count_map_size.c
+++ src/count_map_size.c
@@ -6,7 +6,7 @@
 /*   By: ubuntu <ubuntu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 04:49:22 by ubuntu            #+#    #+#             */
-/*   Updated: 2025/04/28 17:35:10 by ubuntu           ###   ########.fr       */
+/*   Updated: 2025/04/28 18:06:42 by ubuntu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,35 +37,44 @@ static int	check_map_dimensions(t_game *game, int width, int height)
 	return (1);
 }
 
-int	count_map_size(t_game *game, int fd)
+static int	handle_line_error(int fd, char *line)
+{
+	cleanup_gnl_buffer(fd);
+	free(line);
+	return (0);
+}
+
+static int	read_map_lines(int fd, int *width, int *height)
 {
 	char	*line;
-	int		width;
-	int		height;
 
-	height = 0;
-	width = 0;
+	*height = 0;
+	*width = 0;
 	line = get_next_line(fd);
 	if (!line)
-	{
-		cleanup_gnl_buffer(fd);
-		return (ft_printf("Error: Empty map file\n"), 0);
-	}
+		return (cleanup_gnl_buffer(fd),
+			ft_printf("Error: Empty map file\n"), 0);
 	while (line)
 	{
 		if (ft_strchr(line, '\n'))
 			*ft_strchr(line, '\n') = '\0';
-		width = check_line_width(line, width, height);
-		if (width == -1)
-		{
-			cleanup_gnl_buffer(fd);
-			free(line);
-			return (0);
-		}
-		height++;
+		*width = check_line_width(line, *width, *height);
+		if (*width == -1)
+			return (handle_line_error(fd, line));
+		(*height)++;
 		free(line);
 		line = get_next_line(fd);
 	}
 	cleanup_gnl_buffer(fd);
+	return (1);
+}
+
+int	count_map_size(t_game *game, int fd)
+{
+	int	width;
+	int	height;
+
+	if (!read_map_lines(fd, &width, &height))
+		return (0);
 	return (check_map_dimensions(game, width, height));
 }
